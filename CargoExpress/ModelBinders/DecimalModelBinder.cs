fc406fbb86d3d4ea1e312c5cbc23a0015aae57f0
jzlatev <52425187+jzlatev@users.noreply.github.com>
2022-03-12 15:54:00 +0200

    Add custom decimal modelBinder

---
+++ CargoExpress/ModelBinders/DecimalModelBinder.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Globalization;
+
+namespace CargoExpress.ModelBinders
+{
+    public class DecimalModelBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            ValueProviderResult valueResult = bindingContext.ValueProvider.GetValue(bindingContext.ModelName);
+
+            if (valueResult != ValueProviderResult.None && !String.IsNullOrEmpty(valueResult.FirstValue))
+            {
+                decimal value = 0;
+                bool isDecimal = false;
+
+                try
+                {
+                    string currentValue = valueResult.FirstValue;
+                    currentValue = currentValue.Replace(".", CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator);
+                    currentValue = currentValue.Replace(",", CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator);
+
+                    value = Convert.ToDecimal(currentValue, CultureInfo.CurrentCulture);
+                    isDecimal = true;
+                }
+                catch (FormatException fe)
+                {
+                    bindingContext.ModelState.AddModelError(bindingContext.ModelName, fe, bindingContext.ModelMetadata);
+                }
+
+                if (isDecimal)
+                {
+                    bindingContext.Result = ModelBindingResult.Success(value);
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
--- CargoExpress/Program.cs
+++ CargoExpress/Program.cs
@@ -1,4 +1,5 @@
 using CargoExpress.Data;
+using CargoExpress.ModelBinders;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,9 +11,13 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
     .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews()
+    .AddMvcOptions(option =>
+    {
+        option.ModelBinderProviders.Insert(0, new DecimalModelBinderProvider());
+    });
 
 var app = builder.Build();
 
