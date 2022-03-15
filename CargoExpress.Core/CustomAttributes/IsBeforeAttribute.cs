24d173e8e3b8496cecff193fda291626523db239
jzlatev <52425187+jzlatev@users.noreply.github.com>
2022-03-15 12:32:08 +0200

    Added IsBefore validation custom attribute to check whether passed date is before another date.

---
+++ CargoExpress.Core/CustomAttributes/IsBeforeAttribute.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CargoExpress.Core.CustomAttributes
+{
+    public class IsBeforeAttribute : ValidationAttribute
+    {
+        private readonly string date;
+
+        public IsBeforeAttribute(string _date, string errorMessage = "")
+        {
+            date = _date;
+            ErrorMessage = errorMessage;
+        }
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            try
+            {
+                DateTime dateToCompare = (DateTime)validationContext
+                    .ObjectType
+                    .GetProperty(date)
+                    .GetValue(validationContext.ObjectInstance);
+            
+                if ((DateTime)value < dateToCompare)
+                {
+                    return ValidationResult.Success;
+                }
+            }
+            catch (Exception)
+            {}
+            
+            return new ValidationResult(ErrorMessage);
+        }
+    }
+}
