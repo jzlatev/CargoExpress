cd6e18a796ef55ace4d5193e1a5f1904b8374227
jzlatev <52425187+jzlatev@users.noreply.github.com>
2022-03-20 22:41:35 +0200

    Created extended services class.

---
+++ CargoExpress/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,26 @@
+﻿using CargoExpress.Infrastructure.Data;
+using CargoExpress.Infrastructure.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddApplicationService(this IServiceCollection services)
+        {
+            services.AddScoped<IApplicationDbRepository, ApplicationDbRepository>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddApplicationDbContext(this IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            return services;
+        }
+    }
+}
--- CargoExpress/Program.cs
+++ CargoExpress/Program.cs
@@ -1,18 +1,12 @@
 using CargoExpress.Core.Constants;
 using CargoExpress.Infrastructure.Data;
-using CargoExpress.Infrastructure.Data.Repositories;
 using CargoExpress.ModelBinders;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
+builder.Services.AddApplicationDbContext(builder.Configuration);
 builder.Services.AddDefaultIdentity<IdentityUser>(options =>
     {
         options.SignIn.RequireConfirmedAccount = false;
@@ -27,7 +21,7 @@ builder.Services.AddControllersWithViews()
         option.ModelBinderProviders.Insert(2, new DoubleModelBinderProvider());
     });
 
-builder.Services.AddScoped<IApplicationDbRepository, ApplicationDbRepository>();
+builder.Services.AddApplicationService();
 
 var app = builder.Build();
 
